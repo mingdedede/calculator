5402759492ff5543282dfccded74664c6e8b9fdf
Jim Pepe <jim.pepe@verizon.net>
2016-01-04 17:13:18 -0500

    Initial commit

---
+++ app/src/main/java/com/course/example/LayoutDemo.java
@@ -0,0 +1,19 @@
+package com.course.example;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutDemo extends Activity {
+    /** demonstrates several layouts */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_linearlayout);
+        //setContentView(R.layout.main_relative);
+        //setContentView(R.layout.main_gridlayout);
+        //setContentView(R.layout.main_tablelayout);
+        //setContentView(R.layout.main_scrollview);
+        //setContentView(R.layout.main_absolutelayout);
+    }
+}
